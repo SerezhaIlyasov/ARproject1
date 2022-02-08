2f9e972e57c8097452f89f927996de573ae155ab
SerezhaIlyasov <98167998+SerezhaIlyasov@users.noreply.github.com>
2022-02-08 15:13:03 +0300

    Lesson 3 full

--- Assets/Scripts/ShowMenu.cs
+++ Assets/Scripts/ShowMenu.cs
@@ -1,36 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowMenu : MonoBehaviour
 {
     public GameObject [] mainMenu;
+    float animDuration = 0.5f;
 
     private void Start()
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < mainMenu.Length; i++)
         {
             mainMenu[i].SetActive(false);
         }
     }
     public void Show()
     {
-        for (int i = 0; i < 2; i++)
-        {
-            mainMenu[i].SetActive(true);
-        }
+        StartCoroutine(FadeIn());
     }
 
 
     public void Hide()
     {
-        for (int i = 0; i < 2; i++)
-        {
-            mainMenu[i].SetActive(false);
-        }
+        StartCoroutine(FadeOut());
     }
 
+    IEnumerator FadeOut()
+    {
+        for (float i = 0f; i <= 1f; i += Time.deltaTime / animDuration)
+        {
+            foreach (GameObject button in mainMenu)
+            {
+                button.GetComponent<Image>().color = Color.Lerp(Color.white, Color.clear, i);
+            }
+            yield return null;
+        }
+        foreach (GameObject button in mainMenu)
+        {
+            button.SetActive(false);
+        }
 
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float i = 0f; i <= 1f; i += Time.deltaTime / animDuration)
+        {
+            foreach (GameObject buttons in mainMenu)
+            {
+                buttons.GetComponent<Image>().color = Color.Lerp(Color.clear, Color.white, i);
+            }
+            yield return null;
+        }
+        foreach (GameObject button in mainMenu)
+        {
+            button.SetActive(true);
+        }
+    }
 
 
 
